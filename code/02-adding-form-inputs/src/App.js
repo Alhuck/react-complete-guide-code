e91b4fd9d37598540ffb645b73f475fda2e0ae9c
Alhuck <alhucka@gmail.com>
2021-04-22 17:41:19 -0400

    Added some conditional rendering

--- code/02-adding-form-inputs/src/App.js
+++ code/02-adding-form-inputs/src/App.js
@@ -7,20 +7,20 @@ const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
-      amount: 94.12,
+      cost: 94.12,
       date: new Date(2020, 7, 14),
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
-      amount: 294.67,
+      cost: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
-      amount: 450,
+      cost: 450,
       date: new Date(2021, 5, 12),
     },
   ];
@@ -29,14 +29,25 @@ const DUMMY_EXPENSES = [
 const App = () => {
 
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [showForm, setShowForm] = useState(false);
 
   const getNewExpenseForm = (newExpense) => {
-    setExpenses(preExpenses => [newExpense, ...preExpenses])
+    setExpenses((prevExpenses) 
+          => {return [newExpense, ...prevExpenses]})
+  }
+ 
+  const showNewExpenseForm = (status) => {
+    setShowForm(status);
+  }
+
+  let formToRender = <button onClick={() => showNewExpenseForm(true)} className="new-expense">Add new expense</button>;
+  if(showForm) {
+    formToRender = <NewExpense showForm = {(status) => showNewExpenseForm(status)} formSubmit={getNewExpenseForm}/> 
   }
 
   return (
     <div>
-      <NewExpense formSubmit={getNewExpenseForm}/>
+      {formToRender}
       <Expenses items={expenses}/>
     </div>
   );
